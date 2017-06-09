b5585794e67a7f8a72a3afed20fe984ef8e76c91
bret barker <bret@abitrandom.net>
2017-06-09 17:29:29 -0400

    sleep a bit

--- download-docker.sh
+++ download-docker.sh
@@ -10,7 +10,9 @@ nohup tcpdump -fnv -w resets.pcap dst port 443 and 'tcp[tcpflags] & (tcp-rst) !=
 
 for i in `seq $ATTEMPTS`;
 do curl -w '%{http_code}: %{url_effective} %{size_download} %{time_total}s\n' -LsS \
-  $(curl -s -H 'X-Ubuntu-Series: 16' https://search.apps.ubuntu.com/api/v1/snaps/details/core | jq '.anon_download_url' -r) -o /dev/null;
+    $(curl -s -H 'X-Ubuntu-Series: 16' https://search.apps.ubuntu.com/api/v1/snaps/details/core | jq '.anon_download_url' -r) -o /dev/null;
+    echo "sleeping 5"
+    sleep 5
 done
 
 kill `cat tcpdump.pid`
