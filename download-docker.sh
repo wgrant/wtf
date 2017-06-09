1b3769b4c7281aef11fe46aba0d3f87626dea6da
bret barker <bret@abitrandom.net>
2017-06-09 16:58:33 -0400

    added docker version and mtr traces

--- download-docker.sh
+++ download-docker.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+apt update >> /dev/null && apt install -y curl jq mtr tcpdump >> /dev/null
+
 ATTEMPTS=${1:-10}
 echo "Trying $ATTEMPTS core snap download(s) ..."
 
-nohup sudo tcpdump -fnv -w resets.pcap dst port 443 and 'tcp[tcpflags] & (tcp-rst) != 0' \
+nohup tcpdump -fnv -w resets.pcap dst port 443 and 'tcp[tcpflags] & (tcp-rst) != 0' \
   > tcpdump.out 2>&1 & echo $! > tcpdump.pid
 
 for i in `seq $ATTEMPTS`;
@@ -11,13 +13,13 @@ do curl -w '%{http_code}: %{url_effective} %{size_download} %{time_total}s\n' -L
   $(curl -s -H 'X-Ubuntu-Series: 16' https://search.apps.ubuntu.com/api/v1/snaps/details/core | jq '.anon_download_url' -r) -o /dev/null;
 done
 
-sudo kill `cat tcpdump.pid`
+kill `cat tcpdump.pid`
 
 echo "=RESETS======================================================"
 cat resets.pcap
 echo "=/RESETS====================================================="
 echo
 echo "mtr to internap, ipv4"
-sudo mtr -rbw4 068ed04f23.site.internapcdn.net
+mtr -rbw4 068ed04f23.site.internapcdn.net
 echo "mtr to internap, ipv6"
-sudo mtr -rbw6 068ed04f23.site.internapcdn.net
+mtr -rbw6 068ed04f23.site.internapcdn.net
