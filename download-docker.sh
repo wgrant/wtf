4f65334108b2cd9f91ac1aef7c326c58d4b35ffb
bret barker <bret@abitrandom.net>
2017-06-12 12:40:58 -0400

    use snapcraft's python downloader

--- download-docker.sh
+++ download-docker.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-apt update >> /dev/null && apt install -y curl jq mtr tcpdump >> /dev/null
+apt update >> /dev/null && apt install -y curl jq mtr tcpdump python3-pip >> /dev/null
+python3 -m pip install -r requirements.txt
 
 ATTEMPTS=${1:-10}
 echo "Trying $ATTEMPTS core snap download(s) ..."
@@ -9,10 +10,13 @@ nohup tcpdump -fnv -w resets.pcap dst port 443 and 'tcp[tcpflags] & (tcp-rst) !=
   > tcpdump.out 2>&1 & echo $! > tcpdump.pid
 
 for i in `seq $ATTEMPTS`;
-do curl -w '%{http_code}: %{url_effective} %{size_download} %{time_total}s\n' -LsS \
-    $(curl -s -H 'X-Ubuntu-Series: 16' https://search.apps.ubuntu.com/api/v1/snaps/details/core | jq '.anon_download_url' -r) -o /dev/null;
-    echo "sleeping 5"
-    sleep 5
+#do curl -w '%{http_code}: %{url_effective} %{size_download} %{time_total}s\n' -LsS \
+	 #    $(curl -s -H 'X-Ubuntu-Series: 16' https://search.apps.ubuntu.com/api/v1/snaps/details/core | jq '.anon_download_url' -r) -o /dev/null;
+do python3 download.py
+    ls -lh core.snap
+    rm core.snap
+    #echo "sleeping 5"
+    #sleep 5
 done
 
 kill `cat tcpdump.pid`
