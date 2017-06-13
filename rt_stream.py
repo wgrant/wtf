687a61d259ec5825fefd1781850061009861684e
bret barker <bret@abitrandom.net>
2017-06-12 21:53:09 -0400

    docker, internap https, simple py - real URL

--- rt_stream.py
+++ rt_stream.py
@@ -4,7 +4,8 @@ from requests_toolbelt.downloadutils import stream
 
 
 #url = 'http://abitrandom.net/core.snap'
-url = 'https://7af8351798.site.internapcdn.net/debug/core.snap'
+#url = 'https://7af8351798.site.internapcdn.net/debug/core.snap'
+url = 'https://068ed04f23.site.internapcdn.net/download-snap/99T7MUlRhtI3U0QFgl5mXXESAiSwt776_1689.snap?t=2017-06-13T21:07:40Z&h=95a401792c580e56ab11b61312879fe3b2243296'
 
 print('starting download...')
 r = requests.get(url, stream=True)
