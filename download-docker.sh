56d7a602c1cdb8c7d6bd5d121bd4abfd7b872013
bret barker <bret@abitrandom.net>
2017-06-13 15:20:38 -0400

    docker, 2nd cdn ssl but no sign

--- download-docker.sh
+++ download-docker.sh
@@ -35,8 +35,8 @@ do echo -e "\n>>>>>>>>>>>>>> Get URL From Store"
 #    python3 rt_stream.py $URL_local
 
     #cURL2=${cURL//068ed04f23/f081088235}
-    #cURL2="https://f081088235.site.internapcdn.net/download-snap/99T7MUlRhtI3U0QFgl5mXXESAiSwt776_1689.snap?t=2017-09-02T04:15:00Z&h=2F7D1B11A0#2D6965D2F0352D2EA73F779AE45ECB"
-    cURL2="http://f081088235.site.internapcdn.net/download-snap/99T7MUlRhtI3U0QFgl5mXXESAiSwt776_1689.snap?t=2017-09-02T04:00:00Z&h=CB4A03C5B64242CDC442B50069CB0421F491F7A1"
+    cURL2="https://f081088235.site.internapcdn.net/download-snap/99T7MUlRhtI3U0QFgl5mXXESAiSwt776_1689.snap?t=2017-09-02T04:15:00Z&h=2F7D1B11A0#2D6965D2F0352D2EA73F779AE45ECB"
+    #cURL2="http://f081088235.site.internapcdn.net/download-snap/99T7MUlRhtI3U0QFgl5mXXESAiSwt776_1689.snap?t=2017-09-02T04:00:00Z&h=CB4A03C5B64242CDC442B50069CB0421F491F7A1"
     echo -e "\n>>>>>>>>>>>>>> Now use our other CDN test URL $cURL2"
     #echo -e "\n>>>>>>>>>>>>>> CURL IT"
     #curl -vA "wtf" -w '%{http_code}: %{url_effective} %{size_download} %{time_total}s\n' -LsS -D - -o /dev/null $cURL2
