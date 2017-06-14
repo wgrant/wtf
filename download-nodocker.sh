aeb23e3a6e041a05038733d3c85b72c3c38194ed
bret barker <bret@abitrandom.net>
2017-06-14 13:16:03 -0400

    circle

--- download-nodocker.sh
+++ download-nodocker.sh
@@ -18,8 +18,35 @@ nohup sudo tcpdump -fnv -w resets.pcap dst port 443 and 'tcp[tcpflags] & (tcp-rs
 for i in `seq $ATTEMPTS`;
 #do curl -w '%{http_code}: %{url_effective} %{size_download} %{time_total}s\n' -LsS \
 	 #    $(curl -s -H 'X-Ubuntu-Series: 16' https://search.apps.ubuntu.com/api/v1/snaps/details/core | jq '.anon_download_url' -r) -o /dev/null;
-	 #do python3 download.py
-do python3 rt_stream.py "https://068ed04f23.site.internapcdn.net/download-snap/99T7MUlRhtI3U0QFgl5mXXESAiSwt776_1689.snap?t=2017-06-14T17:39:44Z&h=8a089bce53877655be8a21cccb1cc150283277a9"
+do echo -e "\n>>>>>>>>>>>>>> Get URL From Store"
+   sURL=$(curl -s -H 'X-Ubuntu-Series: 16' https://search.apps.ubuntu.com/api/v1/snaps/details/core | jq '.anon_download_url' -r)
+   echo -e "$sURL"
+   echo -e "Get redirect to CDN"
+
+   # murder all the kittens
+   cURL=$(wget --max-redirect=0 -S -O /dev/null $sURL 2>&1 | grep 'Location:' | grep -v 'following' | cut -c 13-)
+   echo -e "\nGot: $cURL"
+   #echo -e "\n>>>>>>>>>>>>>> CURL it"
+   #curl -vA "wtf" -w '%{http_code}: %{url_effective} %{size_download} %{time_total}s\n' -LsS -D - -o /dev/null "$cURL"
+   echo -e "\n>>>>>>>>>>>>>> Python Stream it"
+   python3 rt_stream.py $cURL
+   rm *.snap
+   #    URL_local="$sURL?cdn=local"
+   #    echo -e "\n>>>>>>>>>>>>>> Next try cdn=local $URL_local"
+   #    echo -e "\n>>>>>>>>>>>>>> CURL it"
+   #    curl -vA "wtf" -w '%{http_code}: %{url_effective} %{size_download} %{time_total}s\n' -LsS -D - -o /dev/null $URL_local
+   #    echo -e "\n>>>>>>>>>>>>>> Python Stream it"
+   #    python3 rt_stream.py $URL_local
+   #    rm *.snap
+   #cURL2=${cURL//068ed04f23/f081088235}
+   cURL2="https://f081088235.site.internapcdn.net/download-snap/99T7MUlRhtI3U0QFgl5mXXESAiSwt776_1689.snap?t=2017-09-02T04:15:00Z&h=2F7D1B11A0#2D6965D2F0352D2EA73F779AE45ECB"
+   #cURL2="http://f081088235.site.internapcdn.net/download-snap/99T7MUlRhtI3U0QFgl5mXXESAiSwt776_1689.snap?t=2017-09-02T04:00:00Z&h=CB4A03C5B64242CDC442B50069CB0421F491F7A1"
+    echo -e "\n>>>>>>>>>>>>>> Now use our other CDN test URL $cURL2"
+    #echo -e "\n>>>>>>>>>>>>>> CURL IT"
+    #curl -vA "wtf" -w '%{http_code}: %{url_effective} %{size_download} %{time_total}s\n' -LsS -D - -o /dev/null $cURL2
+    echo -e "\n>>>>>>>>>>>>>> Python Stream it"
+    python3 rt_stream.py $cURL2
+
     #ls -lh core.snap
     rm core.snap
     #echo "sleeping 5"
