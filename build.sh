6c76b01aa1e1e935340b075d3729246709d31085
William Grant <william.grant@canonical.com>
2017-06-28 18:13:02 +1000

    chroot time?

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt install net-tools
+sudo apt install net-tools jq
 
 echo 0 | sudo tee /proc/sys/net/ipv4/tcp_sack
 
@@ -9,6 +9,26 @@ echo "======= DOCKER START ======="
 docker run -v `pwd`:`pwd` -w `pwd` ubuntu:xenial ./download-docker.sh 2
 
 echo "======= DOCKER END ======="
+
+
+echo "Acquiring Launchpad xenial/amd64 chroot."
+XENIAL_CHROOT_URL=`curl -s https://api.launchpad.net/devel/ubuntu/xenial/amd64/chroot_url | jq . -r`
+wget -O /tmp/xenial.tar.gz $XENIAL_CHROOT_URL
+mkdir /tmp/xenial
+pushd /tmp/xenial
+sudo tar xf /tmp/xenial.tar.gz
+sudo cp /etc/resolv.conf chroot-autobuild/etc/resolv.conf
+echo "deb http://archive.ubuntu.com/ubuntu xenial main universe" > chroot-autobuild/etc/apt/sources.list
+popd
+
+sudo mkdir -p /tmp/xenial/chroot-autobuild/`pwd`
+sudo mount --bind `pwd` /tmp/xenial/chroot-autobuild/`pwd`
+sudo chroot /tmp/xenial/chroot-autobuild apt update
+
+echo "======= CHROOT START ======"
+sudo chroot /tmp/xenial/chroot-autobuild bash -c "cd `pwd`; ./download-docker.sh 2"
+echo "======= CHROOT END ======"
+
 
 echo "======= netstat -i ======="
 netstat -i
