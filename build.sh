e4c154cb8823f83c4743435102f7eecd31737542
William Grant <william.grant@canonical.com>
2017-06-28 17:52:47 +1000

    Disable SACK on the host and capture host netstats.

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+sudo apt install net-tools
+
+echo 0 | sudo tee /proc/sys/net/ipv4/tcp_sack
+
+echo "======= DOCKER START ======="
+
+docker run -v `pwd`:`pwd` -w `pwd` ubuntu:xenial ./download-docker.sh 10
+
+echo "======= DOCKER END ======="
+
+echo "======= netstat -i ======="
+netstat -i
+
+echo "======= netstat -s ======="
+netstat -s
