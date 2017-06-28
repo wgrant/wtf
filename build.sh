b8c5375c08d0b6cff2e2f58d846d31f4416c5f67
William Grant <william.grant@canonical.com>
2017-06-28 18:36:10 +1000

    Disable docker for quicker iterations.

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ echo 0 | sudo tee /proc/sys/net/ipv4/tcp_sack
 
 echo "======= DOCKER START ======="
 
-docker run -v `pwd`:`pwd` -w `pwd` ubuntu:xenial ./download-docker.sh 2
+#docker run -v `pwd`:`pwd` -w `pwd` ubuntu:xenial ./download-docker.sh 2
 
 echo "======= DOCKER END ======="
 
