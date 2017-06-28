a98fd0767e2a8f5a75dff9c8a214a98bd6ea73fb
William Grant <william.grant@canonical.com>
2017-06-28 19:42:52 +1000

    Embiggen buffers.

--- build.sh
+++ build.sh
@@ -5,6 +5,10 @@ export LC_ALL=C
 sudo apt install net-tools jq
 
 echo 0 | sudo tee /proc/sys/net/ipv4/tcp_sack
+echo 16777216 | sudo tee /proc/sys/net/core/rmem_default
+echo 16777216 | sudo tee /proc/sys/net/core/rmem_max
+echo "16777216 16777216 16777216" | sudo tee /proc/sys/net/ipv4/tcp_mem
+echo "16777216 16777216 16777216" | sudo tee /proc/sys/net/ipv4/tcp_rmem
 
 echo "======= DOCKER START ======="
 ./do-docker.sh "./download-docker.sh 2"
